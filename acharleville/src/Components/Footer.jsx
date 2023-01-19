402259b1f9cef5749f7d362c66431917d6ab83b0
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-01-19 15:55:51 +0100

    feat: footer component

---
+++ acharleville/src/Components/Footer.jsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return (
+      <footer className="bg-gray-800 py-2">
+        <div className="container mx-auto flex items-center justify-between flex-wrap">
+          <div className="flex justify-center w-full px-4 py-6 text-white">
+            <a className="px-4 py-4 text-sm m-2 bg-white rounded-full p-3" href="https://www.instagram.com/pellarinalizee/">
+            <img src="https://cdn-icons-png.flaticon.com/512/2111/2111679.png" alt="insta" className="w-12 h-12"/>
+            </a>
+            <a className="px-4 py-4 text-sm m-2 bg-white rounded-full p-3" href="https://www.instagram.com/pellarinalizee/">
+            <img src="https://cdn-icons-png.flaticon.com/512/2582/2582637.png" alt="insta" className="w-12 h-12"/>
+            </a>
+            <a className="px-4 py-4 text-sm m-2 bg-white rounded-full p-3" href="https://www.instagram.com/pellarinalizee/">
+            <img src="https://cdn-icons-png.flaticon.com/512/2582/2582575.png" alt="insta" className="w-12 h-12"/>
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+  };
+
+export default Footer;
