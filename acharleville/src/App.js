ce821a945283e955785dfa6d49584006b45df934
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-01-20 01:18:28 +0100

    feat: updated app

--- acharleville/src/App.js
+++ acharleville/src/App.js
@@ -1,8 +1,20 @@
 import "./App.css"
+import Header from "./Components/Header"
+import Avatar from "./Components/Avatar";
 
 const App = () => {
   return (
-    <div>Hello World</div>
+    <div>
+      <Header />
+      <Avatar />
+      <div>
+        Salut je suis Alizée et je suis
+        <div class="text-xl font-semibold
+            bg-gradient-to-r bg-clip-text  text-transparent
+            from-indigo-500 via-purple-500 to-indigo-500
+            animate-text">SoundDesigneuse</div>
+      </div>
+    </div>
   );
 }
 
