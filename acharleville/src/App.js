25fbb2f4da02eec5f0b30cf201c416a29f5c0693
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-01-22 20:07:38 +0100

    feat: updated main

--- acharleville/src/App.js
+++ acharleville/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css"
 import Header from "./Components/Header"
 import Avatar from "./Components/Avatar";
+import Button from "./Components/Button";
 
 const App = () => {
   return (
@@ -9,10 +10,8 @@ const App = () => {
       <Avatar />
       <div>
         Salut je suis Alizée et je suis
-        <div class="text-xl font-semibold
-            bg-gradient-to-r bg-clip-text  text-transparent
-            from-indigo-500 via-purple-500 to-indigo-500
-            animate-text">SoundDesigneuse</div>
+        <div class="text-xl font-semibold bg-gradient-to-r bg-clip-text  text-transparent from-indigo-500 via-purple-500 to-indigo-500 animate-text">SoundDesigneuse</div>
+        <Button txt="Un coup d'oeil à mon CV"/>
       </div>
     </div>
   );
