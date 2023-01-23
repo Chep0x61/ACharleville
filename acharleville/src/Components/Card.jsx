485ebfac7f20bb4152ce9bcad839a92d41749449
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-01-23 17:48:05 +0100

    feat: card component with popup

--- acharleville/src/Components/Card.jsx
+++ acharleville/src/Components/Card.jsx
@@ -1,3 +1,6 @@
+import PopUp from "./PopUp";
+import PopUpContent from "./PopUpContent"
+
 const Card = ({ w, h, title, desc, pic, btn }) => {
     return (
       <div className={`card w-${w} h-${h} bg-base-100 shadow-xl`}>
@@ -6,7 +9,8 @@ const Card = ({ w, h, title, desc, pic, btn }) => {
           <h2 className="card-title">{title}</h2>
           <p>{desc}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">{btn}</button>
+            <PopUpContent />
+            <PopUp />
           </div>
         </div>
       </div>
