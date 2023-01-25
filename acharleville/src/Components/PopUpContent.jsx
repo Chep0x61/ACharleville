5b6edadd6e02b0c3b219b70f86d81a5c9fecec45
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-01-25 14:02:06 +0100

    refactor: popup component

--- acharleville/src/Components/PopUpContent.jsx
+++ acharleville/src/Components/PopUpContent.jsx
@@ -1,17 +1,20 @@
 const PopupContent = ({ id }) => {
     return (
         <div>
-        <input type="checkbox" id="my-modal-1" className="modal-toggle" />
-        <div className="modal modal-bottom sm:modal-middle">
-          <div className="modal-box">
-            <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
-            <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-            <figure><img src="https://placeimg.com/400/225/arch" alt="awe" /></figure>
-            <div className="modal-action">
-              <label htmlFor="my-modal-6" className="btn">Yay!</label>
-            </div>
-          </div>
-        </div>
+<input type="checkbox" id="my-modal-1" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box w-11/12 max-w-7xl items-center justify-center">
+    <label htmlFor="my-modal-1" className="btn btn-sm btn-circle absolute right-2 top-2">x</label>
+    <h3 className="font-bold text-lg pl-12">Titre du projet</h3>
+    <div className="px-12 py-4">
+    <iframe width="1280" height="720" src="https://www.youtube.com/embed/MUwnVA6GNXo?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+    <p className="pl-12">Ceci est une descri^tion du projet que peu de gens liront Ceci est une descri^tion du projet que peu de gens liront Ceci est une descri^tion du projet que peu de gens liront</p>
+    <div className="modal-action">
+      <label htmlFor="my-modal-1" className="btn">Yay!</label>
+    </div>
+  </div>
+</div>
         </div>
     );
 }
