3214ffc8609d9998cf83325c8a76f13a0faf73ab
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-01-25 18:24:48 +0100

    feat: change main

--- acharleville/src/App.js
+++ acharleville/src/App.js
@@ -1,18 +1,15 @@
 import "./App.css"
 import Header from "./Components/Header"
-import Avatar from "./Components/Avatar";
 import Button from "./Components/Button";
 import Card from "./Components/Card"
+import Hero from "./Components/Hero";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Avatar />
+      <Hero />
       <div>
-        Salut je suis Alizée et je suis
-        <div class="text-xl font-semibold bg-gradient-to-r bg-clip-text  text-transparent from-indigo-500 via-purple-500 to-indigo-500 animate-text">SoundDesigneuse</div>
-        <Button txt="Un coup d'oeil à mon CV"/>
         <Card w="96" title="MacNCheese" desc="description" pic="https://m.media-amazon.com/images/M/MV5BMDRhYzUwZGQtMTRkMi00NzMwLWE0ZjMtMzRiYzFjZWQwMzIxXkEyXkFqcGdeQXVyMjEzNTA4MTM@._V1_.jpg"/>
       </div>
     </div>
