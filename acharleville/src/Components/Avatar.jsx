b3d65b8fd67b8f5d591342a2ac91b52e99171f31
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-01-25 18:23:16 +0100

    feat: avatar component

--- acharleville/src/Components/Avatar.jsx
+++ acharleville/src/Components/Avatar.jsx
@@ -1,7 +1,7 @@
 const Avatar = () => {
     return (
-        <div className="avatar">
-            <div className="w-80 rounded-full ring ring-secondary ring-offset-base-100 ring-offset-2">
+        <div className="avatar pr-12">
+            <div className="shadow-2xl w-96 rounded-full ring ring-secondary ring-offset-base-100 ring-offset-2">
                 <img src="https://placeimg.com/192/192/people" />
             </div>
         </div>
