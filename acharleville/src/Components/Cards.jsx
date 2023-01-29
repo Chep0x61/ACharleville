874542adf8f2fa5414fba2d7d3b116dc7b3091e1
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-01-29 22:47:52 +0100

    fix: typo in imports

--- acharleville/src/Components/Cards.jsx
+++ acharleville/src/Components/Cards.jsx
@@ -1,14 +1,14 @@
-import Card from "./Card"
+import Card from "./Card";
 
 const Cards = () => {
     return (
         <div>
             <h2 className="text-4xl font-bold text-center pb-8 pt-8">Mes Projets</h2>
             <div className="grid grid-cols-3 gap-none place-content-center place-items-center">
-                <Card w="96" title="MacNCheese" desc="description" pic="https://france3-regions.francetvinfo.fr/image/m-ZkYkyqgurN_90Ve0wJKRhWaW4/1200x900/regions/2020/06/09/5edf64fae2524_castor_-_pixabay-3752051.jpg"/>
-                <Card w="96" title="MacNCheese" desc="description" pic="https://france3-regions.francetvinfo.fr/image/m-ZkYkyqgurN_90Ve0wJKRhWaW4/1200x900/regions/2020/06/09/5edf64fae2524_castor_-_pixabay-3752051.jpg"/>
-                <Card w="96" title="MacNCheese" desc="description" pic="https://france3-regions.francetvinfo.fr/image/m-ZkYkyqgurN_90Ve0wJKRhWaW4/1200x900/regions/2020/06/09/5edf64fae2524_castor_-_pixabay-3752051.jpg"/>
-                <Card w="96" title="MacNCheese" desc="description" pic="https://france3-regions.francetvinfo.fr/image/m-ZkYkyqgurN_90Ve0wJKRhWaW4/1200x900/regions/2020/06/09/5edf64fae2524_castor_-_pixabay-3752051.jpg"/>
+                <Card w="96" title="MacNCheese" desc="description" pic="https://france3-regions.francetvinfo.fr/image/m-ZkYkyqgurN_90Ve0wJKRhWaW4/1200x900/regions/2020/06/09/5edf64fae2524_castor_-_pixabay-3752051.jpg" alt="castor"/>
+                <Card w="96" title="MacNCheese" desc="description" pic="https://france3-regions.francetvinfo.fr/image/m-ZkYkyqgurN_90Ve0wJKRhWaW4/1200x900/regions/2020/06/09/5edf64fae2524_castor_-_pixabay-3752051.jpg" alt="castor"/>
+                <Card w="96" title="MacNCheese" desc="description" pic="https://france3-regions.francetvinfo.fr/image/m-ZkYkyqgurN_90Ve0wJKRhWaW4/1200x900/regions/2020/06/09/5edf64fae2524_castor_-_pixabay-3752051.jpg" alt="castor"/>
+                <Card w="96" title="MacNCheese" desc="description" pic="https://france3-regions.francetvinfo.fr/image/m-ZkYkyqgurN_90Ve0wJKRhWaW4/1200x900/regions/2020/06/09/5edf64fae2524_castor_-_pixabay-3752051.jpg" alt="castor"/>
             </div>
         </div>
     );
