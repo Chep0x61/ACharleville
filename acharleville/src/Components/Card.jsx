9ecb4165567bf1fbe7f68284a694a9b161b037e2
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-01-29 13:38:17 +0100

    feat: card component pic dimensions

--- acharleville/src/Components/Card.jsx
+++ acharleville/src/Components/Card.jsx
@@ -3,8 +3,8 @@ import PopUpContent from "./PopUpContent"
 
 const Card = ({ w, h, title, desc, pic, btn }) => {
     return (
-      <div className={`card w-${w} h-${h} bg-base-100 shadow-xl`}>
-        <figure><img src={pic} alt="Picture" /></figure>
+      <div className={`card card-compact w-${w} h-${h} bg-base-100 shadow-xl`}>
+        <figure className="max-w-full max-h-[50%]"><img src={pic} alt="Picture" className="h-auto max-w-full"/></figure>
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
           <p>{desc}</p>
