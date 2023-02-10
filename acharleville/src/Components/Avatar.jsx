81f7608ef6c2a6c938d4e967b0bc1e64339ad1f5
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-02-10 11:17:54 +0100

    refactor: mobile first avatar

--- acharleville/src/Components/Avatar.jsx
+++ acharleville/src/Components/Avatar.jsx
@@ -1,8 +1,8 @@
 const Avatar = () => {
     return (
         <div className="avatar pr-12">
-            <div className="shadow-2xl w-96 rounded-full ring ring-secondary ring-offset-base-100 ring-offset-2">
-                <img src="https://placeimg.com/192/192/people" />
+            <div className="shadow-2xl w-48 rounded-full ring ring-secondary ring-offset-base-100 ring-offset-2">
+                <img src="/images/alizee.jpg" />
             </div>
         </div>
     );
