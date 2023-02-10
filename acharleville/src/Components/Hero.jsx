246dbea4a42274b1914e9d292ce27dd387f2ddc0
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-02-10 15:07:35 +0100

    feat: mobile first hero component

--- acharleville/src/Components/Hero.jsx
+++ acharleville/src/Components/Hero.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 
 const Hero = () => {
     const [index, setIndex] = useState(0);
-    const texts = ["I'm a Sound Designer", "I'm a developer", "I'm a videast", "I'm a marketer"];
+    const texts = ["Sound Designer", "Perchman", "Mixeur"];
     const [text, setText] = useState(texts[index]);
 
     useEffect(() => {
@@ -16,18 +16,20 @@ const Hero = () => {
     });
 
     return(
-        <div className="hero min-h-screen bg-base-200 bg-[url('../public/oooscillate.svg')]">
-            <div className="hero-content flex-col lg:flex-row pl-14">
+        <div className="hero min-h-screen bg-base-200 bg-[url('../public/oooscillate.svg')] bg-cover pb-20 md:pb-12 lg:pb-none">
+            <div className="hero-content flex-col lg:flex-row">
             <Avatar />
-            <div>
-              <h1 className="text-4xl font-bold py-2 pl-24 ">Salut, moi c'est Alizée !</h1>
-                <div class="text-5xl font-semibold bg-gradient-to-r bg-clip-text  text-transparent from-indigo-500 via-purple-500 to-indigo-500 animate-text pl-24">Je suis {text}</div>
-                <div className="pt-12 pl-72">
-                    <Button txt="Retrouvez mon CV"/>
-                </div>
+            <div className="flex flex-col justify-center items-center lg:pl-8 lg:gap-2">
+              <p className='text-2xl font-bold text-center md:text-4xl lg:text-5xl'>Salut, je suis Alizée!</p>
+              <p className="text-2xl font-semibold text-center bg-gradient-to-r bg-clip-text text-transparent from-indigo-500 via-purple-500 to-indigo-500 animate-text">Je suis un Sound Designer</p>
+              <div className="p-8">
+              <a href="/documents/example.pdf" target="_blank" rel="noopener noreferrer">
+                <button className="btn btn-secondary">Retrouvez mon CV</button>
+              </a>
               </div>
             </div>
           </div>
+        </div>
     );
 }
 
