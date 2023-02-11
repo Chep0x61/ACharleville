b75be872e9f97427c735510c561e89bb3ad10b0f
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-02-11 02:44:00 +0100

    refactor: mobile first popupcontent video

--- acharleville/src/Components/PopUpContent.jsx
+++ acharleville/src/Components/PopUpContent.jsx
@@ -1,18 +1,33 @@
-const PopupContent = ({ id }) => {
+import { useEffect, useState } from "react";
+
+const PopupContent = ({ id, title, description, ytitle, ylink }) => {
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+      };
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    const iframeWidth = width < 400 ? 300 : 700;
+    const iframeHeight = height < 350 ? 150 : 400;
+
+
     return (
         <div>
-          <input type="checkbox" id="my-modal-1" className="modal-toggle" />
+          <input type="checkbox" id="my-modal-${id}" className="modal-toggle" />
           <div className="modal">
-            <div className="modal-box w-11/12 max-w-7xl">
-              <label htmlFor="my-modal-1" className="btn btn-sm btn-circle absolute right-4 top-4">x</label>
-              <h3 className="font-bold text-lg pl-12">Titre du projet</h3>
-              <div className="px-12 py-4">
-                <iframe width="1280" height="720" src="https://www.youtube.com/embed/MUwnVA6GNXo?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div className="modal-box w-[90%] max-w-4xl">
+              <label htmlFor="my-modal-${id}" className="btn btn-sm btn-circle absolute right-2 top-2">x</label>
+              <h3 className="font-bold text-lg">{title}</h3>
+              <div className="flex justify-center">
+              <iframe width={iframeWidth} height={iframeHeight} src={ylink} title={ytitle} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
               </div>
-              <p className="pl-12">Ceci est une descri^tion du projet que peu de gens liront Ceci est une descri^tion du projet que peu de gens liront Ceci est une descri^tion du projet que peu de gens liront</p>
-              <div className="modal-action">
-                <label htmlFor="my-modal-1" className="btn">Yay!</label>
-              </div>
+              <p className="">{description}</p>
             </div>
           </div>
         </div>
