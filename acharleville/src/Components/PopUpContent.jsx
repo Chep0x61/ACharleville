e8f4a70de9cd0032a1ec9bd81f6cee42fdbaddfd
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-02-11 23:44:36 +0100

    feat: new props in PopUpContent

--- acharleville/src/Components/PopUpContent.jsx
+++ acharleville/src/Components/PopUpContent.jsx
@@ -19,10 +19,10 @@ const PopupContent = ({ id, title, description, ytitle, ylink }) => {
 
     return (
         <div>
-          <input type="checkbox" id="my-modal-${id}" className="modal-toggle" />
+          <input type="checkbox" id={`my-modal-${id}`} className="modal-toggle" />
           <div className="modal">
             <div className="modal-box w-[90%] max-w-4xl">
-              <label htmlFor="my-modal-${id}" className="btn btn-sm btn-circle absolute right-2 top-2">x</label>
+              <label htmlFor={`my-modal-${id}`} className="btn btn-sm btn-circle absolute right-2 top-2">x</label>
               <h3 className="font-bold text-lg">{title}</h3>
               <div className="flex justify-center">
               <iframe width={iframeWidth} height={iframeHeight} src={ylink} title={ytitle} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
