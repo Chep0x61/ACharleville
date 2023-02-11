3839a8b84af92cd31ae0d79c07273fbf4c3406ce
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-02-11 23:46:14 +0100

    feat: card component

--- acharleville/src/Components/Card.jsx
+++ acharleville/src/Components/Card.jsx
@@ -1,7 +1,7 @@
 import PopUp from "./PopUp";
 import PopUpContent from "./PopUpContent";
 
-const Card = ({ w, h, title, desc, pic, alt, ylink, ytitle }) => {
+const Card = ({ w, h, title, desc, pic, alt, ylink, ytitle, id }) => {
     return (
       <div className={`card card-compact w-${w} h-${h} bg-base-100 shadow-xl`}>
         <figure className="max-w-full max-h-[50%]"><img src={pic} alt={alt} className="h-auto max-w-full"/></figure>
@@ -9,8 +9,8 @@ const Card = ({ w, h, title, desc, pic, alt, ylink, ytitle }) => {
           <h2 className="card-title">{title}</h2>
           <p>{desc}</p>
           <div className="card-actions justify-end">
-            <PopUpContent id="1" title="Projet 1" description="description" ylink="https://www.youtube.com/embed/X9at37tsrY8" ytitle="resonance sped up"/>
-            <PopUp id="1" />
+            <PopUpContent id={id} title={title} description={desc} ylink={ylink} ytitle={ytitle}/>
+            <PopUp id={id} />
           </div>
         </div>
       </div>
