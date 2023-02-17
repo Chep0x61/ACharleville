90f78b4adbdf0cb74b68a1fedd91e1af86125f77
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-02-17 12:46:46 +0100

    feat: setup contact section

--- acharleville/src/Components/Contact.jsx
+++ acharleville/src/Components/Contact.jsx
@@ -7,7 +7,10 @@ const Contact = () => {
             <div className="hero-content text-center text-neutral-content">
               <div className="card w-[80%] max-w-xl md:max-w-2xl lg:max-w-2xl bg-blue-500 shadow-xl border-8 border-blue-500">
                 <h1 className="mb-5 text-3xl md:text-4xl lg:text-5xl font-bold">Me Contacter</h1>
-                <p className="mb-5 text-md md:text-lg lg:text-xl">Mon profil vous intéresse ? Vous avez une idée de Projet ? On une simple envie de discuter du monde de la création sonore ? Je suis activement à la recherche de travail alors n'hésitez pas !</p>
+                <p className="mt-5 text-md md:text-lg lg:text-xl">Mon profil vous intéresse ?</p>
+                <p className="text-md md:text-lg lg:text-xl">Vous avez une idée de Projet ?</p>
+                <p className="text-md md:text-lg lg:text-xl">Envie de discuter du monde de la création sonore ?</p>
+                <p className="mb-5 text-md md:text-lg lg:text-xl">Je suis ouverte à toute opportunité ! N'hésitez pas</p>
                   <div className="flex justify-center gap-2">
                     <a className="btn btn-circle" href="https://www.instagram.com/pellarinalizee" target="_blank" rel="noreferrer">
                       <img className="h-8 w-8" src="/images/instagram.png"></img>
