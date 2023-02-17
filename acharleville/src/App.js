9b9025f73cd3e00f12ce8131bf4972026db921d7
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-02-17 12:47:09 +0100

    feat: setup sections

--- acharleville/src/App.js
+++ acharleville/src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Cards from "./Components/Cards";
 import About from "./Components/About";
+import Contact from "./Components/Contact";
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Cards/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar/>
+        <Hero/>
+        <About/>
+        <Cards/>
+        <Contact/>
+      </div>
+    </BrowserRouter>
   );
 }
 
