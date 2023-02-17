719975c53ebb3d51199c3d29828998d07a557741
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-02-17 11:55:05 +0100

    refactor: footer component

--- acharleville/src/Components/Footer.jsx
+++ acharleville/src/Components/Footer.jsx
@@ -1,21 +1,11 @@
 const Footer = () => {
-    return (
-      <footer className="bg-gray-800 py-2">
-        <div className="container mx-auto flex items-center justify-between flex-wrap">
-          <div className="flex justify-center w-full px-4 py-6 text-white">
-            <a className="px-4 py-4 text-sm m-2 bg-white rounded-full p-3" href="https://www.instagram.com/pellarinalizee/">
-            <img src="https://cdn-icons-png.flaticon.com/512/2111/2111679.png" alt="insta" className="w-12 h-12"/>
+    return(
+        <div className="flex justify-center text-white text-lg">
+            <a className="pt-8 text-sm md:text-md lg:text-lg" href="https://github.com/Chep0x61" target="_blank" rel="noreferrer">
+                <p>Made with 💜 by Chep0x61 !</p>
             </a>
-            <a className="px-4 py-4 text-sm m-2 bg-white rounded-full p-3" href="https://www.instagram.com/pellarinalizee/">
-            <img src="https://cdn-icons-png.flaticon.com/512/2582/2582637.png" alt="insta" className="w-12 h-12"/>
-            </a>
-            <a className="px-4 py-4 text-sm m-2 bg-white rounded-full p-3" href="https://www.instagram.com/pellarinalizee/">
-            <img src="https://cdn-icons-png.flaticon.com/512/2582/2582575.png" alt="insta" className="w-12 h-12"/>
-            </a>
-          </div>
         </div>
-      </footer>
     );
-  };
+}
 
 export default Footer;
