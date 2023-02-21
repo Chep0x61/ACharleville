d47353e7b06e7549d4c6bbfff7e211f47844225a
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-02-21 10:46:50 +0100

    refactor: card component

--- acharleville/src/Components/Card.jsx
+++ acharleville/src/Components/Card.jsx
@@ -1,13 +1,15 @@
 import PopUp from "./PopUp";
 import PopUpContent from "./PopUpContent";
 
-const Card = ({ w, h, title, desc, pic, alt, ylink, ytitle, id }) => {
+const Card = ({ w, h, title, date, cdesc, desc, pic, alt, ylink, ytitle, id }) => {
     return (
       <div className={`card card-compact w-${w} h-${h} bg-base-100 shadow-xl`}>
-        <figure className="max-w-full max-h-[50%]"><img src={pic} alt={alt} className="h-auto max-w-full"/></figure>
+        <figure className="max-w-full max-h-[50%]"><img src={pic} alt={alt} className="w-full max-h-[370px] object-cover"/></figure>
         <div className="card-body">
-          <h2 className="card-title">{title}</h2>
-          <p>{desc}</p>
+          <h2 className="card-title">{title}
+            <div className="badge badge-secondary text-[0.6rem] lg:text-base">{date}</div>
+          </h2>
+          <p>{cdesc}</p>
           <div className="card-actions justify-end">
             <PopUpContent id={id} title={title} description={desc} ylink={ylink} ytitle={ytitle}/>
             <PopUp id={id} />
