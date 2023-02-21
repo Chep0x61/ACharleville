953b2f7921409efb9e5551e385d19bd2a587045e
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-02-21 10:47:23 +0100

    feat: new about and style to center

--- acharleville/src/Components/About.jsx
+++ acharleville/src/Components/About.jsx
@@ -2,8 +2,11 @@ const About = () => {
     return (
         <section id="about">
         <div>
-            <h2 className="text-4xl font-bold text-center pb-8 pt-8">A propos de moi</h2>
-            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+            <h2 className="text-4xl font-bold text-center pt-8">A propos de moi</h2>
+            <p className="lg:text-md p-12">Dans le domaine du son depuis 2019, j'ai suivi plusieurs formations sur les années afin de couvrir tout un panel de la post production sonore, ainsi la responsabilité de la prise son sur plateau. De l'école Ciinécréatis à Studio M Lyon, j'ai acquis en expérience par le biais de stages sur plateau de fiction télévisuel.
+Aujourd'hui je propose mes services en studio, ainsi qu'en tournage. Si le projet propose du mixage voix, ainsi que musical, je serai ravie d'aider en plus ! 
+
+Vous pouvez jeter un oeil à mes derniers projets, et me contacter sur mes réseaux ou par mail, afin d'échanger autour du merveilleux monde audiovisuel.</p>
         </div>
         </section>
     );
