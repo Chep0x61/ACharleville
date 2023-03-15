e326638fb139f6efdd8f7d8c86d5423ff4fe1e0c
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-03-15 20:43:35 +0100

    feat: awesome colors

--- acharleville/tailwind.config.js
+++ acharleville/tailwind.config.js
@@ -2,8 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{jsx,js}"],
   theme: {
+    colors: {
+      'main': '#212529',
+      'yellow': '#ffd000',
+      'night': '#4a4e69',
+      'check': '#3fc66c',
+      'snow': '#f9f7f3',
+      'test': '#d9dcd6',
+      'orange': '#ffaa00',
+      'flash': '#f7b538',
+      'retry': '#353A47',
+    },
     extend: {
-        'animation': {
+      'screens': {
+        '2xl': '1900px',
+        '3xl': '1920px',
+      },
+      'animation': {
           'text':'text 5s ease infinite',
       },
       'keyframes': {
@@ -22,3 +37,4 @@ module.exports = {
   },
   plugins: [require("daisyui")],
 }
+
