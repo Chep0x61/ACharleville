20893fc8fc2291fb4acfc5d6583e008ea38cf36a
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-03-15 20:57:45 +0100

    feat: new heart

--- acharleville/src/Components/Footer.jsx
+++ acharleville/src/Components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return(
         <div className="flex justify-center text-white text-lg">
             <a className="pt-8 text-sm md:text-md lg:text-lg" href="https://github.com/Chep0x61" target="_blank" rel="noreferrer">
-                <p>Made with 💜 by Chep0x61 !</p>
+                <p>Made with ❤️ by Chep0x61 !</p>
             </a>
         </div>
     );
