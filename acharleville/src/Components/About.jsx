236d06f98ebaec6a9fb941ce051dfbf5bb7a468b
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-03-15 21:08:51 +0100

    feat: about component

--- acharleville/src/Components/About.jsx
+++ acharleville/src/Components/About.jsx
@@ -1,12 +1,14 @@
 const About = () => {
     return (
         <section id="about">
-        <div>
-            <h2 className="text-4xl font-bold text-center pt-8">A propos de moi</h2>
-            <p className="lg:text-md p-12">Dans le domaine du son depuis 2019, j'ai suivi plusieurs formations sur les années afin de couvrir tout un panel de la post production sonore, ainsi la responsabilité de la prise son sur plateau. De l'école Ciinécréatis à Studio M Lyon, j'ai acquis en expérience par le biais de stages sur plateau de fiction télévisuel.
-Aujourd'hui je propose mes services en studio, ainsi qu'en tournage. Si le projet propose du mixage voix, ainsi que musical, je serai ravie d'aider en plus ! 
-
-Vous pouvez jeter un oeil à mes derniers projets, et me contacter sur mes réseaux ou par mail, afin d'échanger autour du merveilleux monde audiovisuel.</p>
+        <div className="hero mb-8">
+            <div className="hero-content text-center">
+                <div className="w-[90%] lg:w-[80%] text-test">
+                    <h2 className="text-4xl font-bold text-center pt-8 pb-8 text-flash">A propos de moi</h2>
+                    <p className="mb-3 font-light text-gray-500 dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:mr-3 first-letter:float-left">Dans le domaine du son depuis 2019, j'ai suivi plusieurs formations sur les années afin de couvrir tout un panel de la post production sonore, ainsi la responsabilité de la prise son sur plateau. De l'école Cinécréatis à Studio M Lyon, j'ai acquis en expérience par le biais de stages sur plateau de fiction télévisuel. Aujourd'hui je propose mes services en studio, ainsi qu'en tournage. Si le projet propose du mixage voix, ainsi que musical, je serai ravie d'aider en plus !</p>
+                    <p className="pt-4 font-light text-gray-500 dark:text-gray-400">Vous pouvez jeter un oeil à mes derniers projets, et me contacter sur mes réseaux ou par mail, afin d'échanger autour du merveilleux monde audiovisuel.</p>
+                </div>
+            </div>
         </div>
         </section>
     );
