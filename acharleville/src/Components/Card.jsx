910113096bd0f479fedad1882fc41109982c85ca
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-03-15 21:10:46 +0100

    feat: card components

--- acharleville/src/Components/Card.jsx
+++ acharleville/src/Components/Card.jsx
@@ -3,13 +3,12 @@ import PopUpContent from "./PopUpContent";
 
 const Card = ({ w, h, title, date, cdesc, desc, pic, alt, ylink, ytitle, id }) => {
     return (
-      <div className={`card card-compact w-${w} h-${h} bg-base-100 shadow-xl`}>
-        <figure className="max-w-full max-h-[50%]"><img src={pic} alt={alt} className="w-full max-h-[370px] object-cover"/></figure>
+      <div className={`card card-compact w-${w} h-${h} bg-retry shadow-xl`}>
+        <figure><img src={pic} alt={alt} className="w-full aspect-[16/9] h-full object-cover"/></figure>
         <div className="card-body">
-          <h2 className="card-title">{title}
-            <div className="badge badge-secondary text-[0.6rem] lg:text-base">{date}</div>
+          <h2 className="card-title text-snow">{title}
+            <div className="badge bg-yellow text-[0.6rem] text-retry border-0 lg:text-base">{date}</div>
           </h2>
-          <p>{cdesc}</p>
           <div className="card-actions justify-end">
             <PopUpContent id={id} title={title} description={desc} ylink={ylink} ytitle={ytitle}/>
             <PopUp id={id} />
