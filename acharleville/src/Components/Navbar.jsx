a783f0dd8bcd2499a04e99a52006e32b45fa4437
Thibault Thuillier <thibault.thuillier@epitech.eu>
2023-03-15 21:09:48 +0100

    feat: navbar component

--- acharleville/src/Components/Navbar.jsx
+++ acharleville/src/Components/Navbar.jsx
@@ -1,24 +1,24 @@
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-flash">
           <div className="navbar-start">
-            <a className="btn btn-ghost normal-case text-xl">ACharleville</a>
+            <a className="btn normal-case text-xl bg-main text-snow hover:bg-main">ACharleville</a>
           </div>
           <div className="navbar-center hidden md:flex">
-            <ul className="menu menu-horizontal">
-              <li><a href="#about" onClick={() => document.getElementById('about').scrollIntoView({behavior: 'smooth'})}>A Propos de moi</a></li>
-              <li><a href="#projects" onClick={() => document.getElementById('projects').scrollIntoView({behavior: 'smooth'})}>Mes Projets</a></li>
-              <li><a href="#contact" onClick={() => document.getElementById('contact').scrollIntoView({behavior: 'smooth'})}>Me contacter</a></li>
+            <ul className="menu menu-horizontal gap-2 font-medium">
+              <li><a className="btn normal-case text-lg bg-main rounded-md text-test hover:text-yellow" href="#about" onClick={() => document.getElementById('about')}>A Propos de moi</a></li>
+              <li><a className="btn normal-case text-lg bg-main rounded-md text-test hover:text-yellow hover:bg-main" href="#projects" onClick={() => document.getElementById('projects')}>Mes Projets</a></li>
+              <li><a className="btn normal-case text-lg bg-main rounded-md text-test hover:text-yellow hover:bg-main" href="#contact" onClick={() => document.getElementById('contact')}>Me contacter</a></li>
             </ul>
           </div>
           <div className="navbar-end gap-2">
-            <a className="btn btn-circle" href="https://www.instagram.com/pellarinalizee" target="_blank" rel="noreferrer">
+            <a className="btn btn-circle bg-main" href="https://www.instagram.com/pellarinalizee" target="_blank" rel="noreferrer">
               <img className="h-8 w-8" src="/images/instagram.png"></img>
             </a>
-            <a className="btn btn-circle" href="https://www.linkedin.com/in/alizée-charleville-4a1227185" target="_blank" rel="noreferrer">
+            <a className="btn btn-circle bg-main" href="https://www.linkedin.com/in/alizée-charleville-4a1227185" target="_blank" rel="noreferrer">
               <img className="h-8 w-8" src="/images/linkedin.png"></img>
             </a>
-            <a className="btn btn-circle" href="mailto:ac.charleville@gmail.com" target="_blank" rel="noreferrer">
+            <a className="btn btn-circle bg-main" href="mailto:ac.charleville@gmail.com" target="_blank" rel="noreferrer">
               <img className="h-8 w-8" src="/images/mail.png"></img>
             </a>
           </div>
