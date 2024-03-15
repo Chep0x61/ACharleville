a246f6c022db2da6f72d1ab993fd267320339dd3
Thibault Thuillier <thibault.thuillier@epitech.eu>
2024-03-16 02:16:46 +0800

    fix: video still playing when modal closed

--- acharleville/src/Components/PopUpContent.jsx
+++ acharleville/src/Components/PopUpContent.jsx
@@ -4,6 +4,7 @@ const PopupContent = ({ id, title, description, ytitle, ylink }) => {
     const [width, setWidth] = useState(window.innerWidth);
     const [height, setHeight] = useState(window.innerHeight);
     const popupRef = useRef(null);
+    const iframeRef = useRef(null);
 
     useEffect(() => {
       const handleResize = () => {
@@ -19,6 +20,7 @@ const PopupContent = ({ id, title, description, ytitle, ylink }) => {
         if (popupRef.current && !popupRef.current.contains(event.target)) {
           const checkbox = document.getElementById(`my-modal-${id}`);
           if (checkbox) {
+            stopVideo()
             checkbox.checked = false;
           }
         }
@@ -32,6 +34,14 @@ const PopupContent = ({ id, title, description, ytitle, ylink }) => {
     const iframeWidth = width < 400 ? 300 : 700;
     const iframeHeight = height < 350 ? 150 : 400;
 
+    const stopVideo = () => {
+      if (iframeRef.current) {
+        const iframe = iframeRef.current;
+        const iframeSrc = iframe.src;
+        iframe.src = iframeSrc;
+      }
+    };
+
     return (
         <div>
           <input type="checkbox" id={`my-modal-${id}`} className="modal-toggle" />
@@ -42,7 +52,7 @@ const PopupContent = ({ id, title, description, ytitle, ylink }) => {
               </label>
               <h3 className="font-bold text-center text-lg lg:text-2xl text-snow pb-4">{title}</h3>
               <div className="flex justify-center">
-              <iframe width={iframeWidth} height={iframeHeight} src={ylink} title={ytitle} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+              <iframe ref={iframeRef} id="video" width={iframeWidth} height={iframeHeight} src={ylink} title={ytitle} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
               </div>
               <div className="flex flex-col justify-center items-center">
                 <p className="text-center text-sm text-snow pt-4 w-[75%]">{description}</p>
