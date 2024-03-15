5952b905067cf36aaa684440e3ef5d18aa7a62d1
Thibault Thuillier <thibault.thuillier@epitech.eu>
2024-03-16 00:45:42 +0800

    feat: translation for projects popup

--- acharleville/src/Components/PopUp.jsx
+++ acharleville/src/Components/PopUp.jsx
@@ -1,6 +1,9 @@
+import { useTranslation } from 'react-i18next'
+
 const Popup = ({ id }) => {
+    const { t } = useTranslation();
     return (
-        <label htmlFor={`my-modal-${id}`} className="btn bg-flash text-retry hover:bg-orange">Voir ce projet</label>
+        <label htmlFor={`my-modal-${id}`} className="btn bg-flash text-retry hover:bg-orange">{t('projectbutton')}</label>
     );
 }
 
