b39bf82a25ab79da769af70b29c5c6dbb161899c
Thibault Thuillier <thibault.thuillier@epitech.eu>
2024-03-16 00:45:15 +0800

    feat: translation for contact section

--- acharleville/src/Components/Contact.jsx
+++ acharleville/src/Components/Contact.jsx
@@ -1,15 +1,18 @@
+import { useTranslation } from 'react-i18next'
 import Footer from "./Footer";
 
 const Contact = () => {
+    const { t } = useTranslation();
+
     return (
         <section id="contact">
           <div className="h-36 lg:h-96 bg-[url('../public/images/decoration/wave.svg')] bg-cover"></div>
           <div className="hero bg-flash">
             <div className="hero-content text-center">
               <div className="card w-[80%] max-w-xl md:max-w-2xl lg:max-w-2xl shadow-2xl text-main">
-                <h1 className="mb-5 text-3xl md:text-4xl lg:text-5xl font-bold">Me Contacter</h1>
-                <p className="text-md md:text-lg lg:text-xl">Je suis ouverte à toute opportunité !</p>
-                <p className="mb-5 text-md md:text-lg lg:text-xl">N'hésitez pas à me contacter sur l'un de mes réseaux.</p>
+                <h1 className="mb-5 text-3xl md:text-4xl lg:text-5xl font-bold">{t('contactme')}</h1>
+                <p className="text-md md:text-lg lg:text-xl">{t('contactdescription1')}</p>
+                <p className="mb-5 text-md md:text-lg lg:text-xl">{t('contactdescription2')}</p>
                   <div className="flex justify-center gap-2">
                     <a className="btn btn-circle" href="https://www.instagram.com/pellarinalizee" target="_blank" rel="noreferrer">
                       <img className="h-8 w-8" src="./images/socials/instagram.png" alt="instagram"></img>
