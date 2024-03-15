ddc58ca3ef7081374d1f8a6bce15c89c4c339291
Thibault Thuillier <thibault.thuillier@epitech.eu>
2024-03-16 00:44:52 +0800

    feat: translation for about me section

--- acharleville/src/Components/About.jsx
+++ acharleville/src/Components/About.jsx
@@ -1,12 +1,17 @@
+import { useTranslation } from 'react-i18next'
+
 const About = () => {
+    const { t } = useTranslation();
+
     return (
         <section id="about">
         <div className="hero mb-8">
             <div className="hero-content text-center">
                 <div className="w-[90%] lg:w-[80%] text-test">
-                    <h2 className="text-4xl font-bold text-center pt-8 pb-8 text-flash">A propos de moi</h2>
-                    <p className="mb-3 font-light text-gray-500 dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-line:text-xl first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:mr-3 first-letter:float-left">Dans le domaine du son depuis 2019, j'ai suivi plusieurs formations afin de couvrir tout un panel de la post production sonore, ainsi que la responsabilité de la prise son sur plateau. De l'école Cinécréatis à Studio M Lyon, j'ai acquis en expérience par le biais de stages sur plateau de fiction télévisée. Aujourd'hui je propose mes services en studio, ainsi qu'en tournage. Si le projet propose du mixage voix, ainsi que musical, je serai ravie de rejoindre l'équipe !</p>
-                    <p className="pt-4 font-light text-gray-500 dark:text-gray-400">Vous pouvez jeter un coup d'œil à mes derniers projets, me contacter sur mes réseaux ou par mail, afin d'échanger autour du monde de l'audiovisuel ou celui du jeu vidéo.</p>
+                    <h2 className="text-4xl font-bold text-center pt-8 pb-8 text-flash">{t('aboutme')}</h2>
+                    <p className="mb-3 font-light text-gray-500 dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-line:text-xl first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:mr-3 first-letter:float-left">{t('aboutmedescription')}</p>
+                    <p className="pt-4 font-light text-gray-500 dark:text-gray-400">{t('aboutmeadvice')}</p>
+                    <p className="mt-8 pt-4 font-light text-gray-500 font-semibold dark:text-gray-400">{t('skills')}</p>
                 </div>
             </div>
         </div>
