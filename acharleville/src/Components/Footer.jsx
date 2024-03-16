52d8bf0e4383b4bc6ee9782d3bbce36b825106ff
Thibault Thuillier <thibault.thuillier@epitech.eu>
2024-03-16 12:41:18 +0800

    fix: font size about made with luv sentence

--- acharleville/src/Components/Footer.jsx
+++ acharleville/src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return(
         <div className="flex justify-center text-white text-lg">
-            <a className="pt-8 text-sm md:text-md lg:text-lg" href="https://github.com/Chep0x61" target="_blank" rel="noreferrer">
+            <a className="pt-8 text-sm md:text-md" href="https://github.com/Chep0x61" target="_blank" rel="noreferrer">
                 <p>Made with ❤️ by Chep0x61 !</p>
             </a>
         </div>
