b6d97ae645b79d0168c381226ea5bb1391724e6d
Thibault Thuillier <thibault.thuillier@epitech.eu>
2024-03-16 12:41:41 +0800

    fix: wrong size of wave on md screen

--- acharleville/src/Components/Contact.jsx
+++ acharleville/src/Components/Contact.jsx
@@ -6,11 +6,11 @@ const Contact = () => {
 
     return (
         <section id="contact">
-          <div className="h-36 lg:h-96 bg-[url('../public/images/decoration/wave.svg')] bg-cover"></div>
+          <div className="h-36 md:h-52 lg:h-96 bg-[url('../public/images/decoration/wave.svg')] bg-cover"></div>
           <div className="hero bg-flash">
             <div className="hero-content text-center">
-              <div className="card w-[80%] max-w-xl md:max-w-2xl lg:max-w-2xl shadow-2xl text-main">
-                <h1 className="mb-5 text-3xl md:text-4xl lg:text-5xl font-bold">{t('contactme')}</h1>
+              <div className="card w-[80%] max-w-xl md:max-w-2xl lg:max-w-2xl shadow-2xl text-main p-4">
+                <h1 className="mb-5 text-3xl md:text-4xl font-bold">{t('contactme')}</h1>
                 <p className="text-md md:text-lg lg:text-xl">{t('contactdescription1')}</p>
                 <p className="mb-5 text-md md:text-lg lg:text-xl">{t('contactdescription2')}</p>
                   <div className="flex justify-center gap-2">
